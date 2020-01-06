2f898fb3e202d35a65fa55a3b24b8d3b2fef4810
Robert Rong <robert.rong@agile-solutions.ch>
2020-01-06 16:05:44 +0100

    swith go modules on

--- install.sh
+++ install.sh
@@ -6,7 +6,7 @@ mkdir /root/go
 export GOPATH=/root/go
 export GOBIN=/usr/local/go/bin
 export PATH=$PATH:$(go env GOPATH)/bin
-export GO111MODULE=auto
+export GO111MODULE=on
 go env GOPATH
 
 #curl https://raw.githubusercontent.com/golang/dep/master/install.sh | sh
