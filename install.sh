57c82973bee89d63c6ce470867c5b1ba93c5e079
Robert Rong <robert.rong@agile-solutions.ch>
2020-01-06 12:00:13 +0100

    remove gopath

--- install.sh
+++ install.sh
@@ -10,8 +10,8 @@ go env GOPATH
 
 #curl https://raw.githubusercontent.com/golang/dep/master/install.sh | sh
 
-mkdir -p $GOPATH/src/github.com/agilesolutions/keycloak-operator
-cd $GOPATH/src/github.com/agilesolutions
+mkdir -p /root/src
+cd /root/src
 git clone https://github.com/agilesolutions/keycloak-operator.git
 cd keycloak-operator
 make setup/mod
